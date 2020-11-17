f7aa5e8a5c628c709fa7132d709283a3d1fa503b
TL <TL@gmail.com>
2020-11-18 02:36:33 +0530

    ok

---
+++ ExceptionHandling/src/com/nt/eh/Test2.java
@@ -0,0 +1,11 @@
+package com.nt.eh;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Test2 {
+public static void main(String[] args) throws FileNotFoundException {
+	PrintWriter pw = new PrintWriter("abc.txt");
+	pw.println("hello");
+	}
+}
